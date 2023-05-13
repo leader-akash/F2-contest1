01ad014d196a1b43e9aa7f9b510dc93c9befff03
leader-akash <117633377+leader-akash@users.noreply.github.com>
2023-05-13 21:30:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,48 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   arr.map(obj => {
+        if(obj.marks > 50) {
+            console.log(obj);
+        }
+    })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+   arr.forEach(obj => {
+        if(obj.marks > 50) {
+            console.log(obj);
+        }
+    })
 }
 
 function addData() {
   //Write your code here, just console.log
+   const newStudent = {id:4,name:"susan",age:"20",marks:45};
+    arr.push(newStudent);
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for(let i = 0; i < arr.length; i++) {
+        if(arr[i].marks < 50) {
+            arr.splice(i, 1);
+            i--;
+        }
+    }
+    console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArr = [
+        { id: 4, name: "dan", age: "23", marks: 53 },
+        { id: 5, name: "jackie", age: "15", marks: 37 },
+        { id: 6, name: "olga", age: "18", marks: 98 },
+      ];
+
+    arr.push(...newArr);
+    console.log(arr);
 }
